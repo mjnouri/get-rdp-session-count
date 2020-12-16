5cc109d4b9e97f5c815338bf9bf68f9b1f7093ef
Mark Nouri <mjnouri@gmail.com>
2020-12-16 00:07:45 -0500

    updated to use statsd

---
+++ get-rdp-session-count-old.ps1
@@ -0,0 +1,30 @@
+# This method used 85% CPU for 3 seconds. Scripts are also no longer recommended by AWS. Check get-rdp-session-count.ps1 for use with statsd.
+
+qwinsta /counter | Select-String -Pattern 'Disc|Active' | Select-String -Pattern 'services|sessions' -NotMatch > C:\rdp-total-session-count.log
+$line_count = Get-Content C:\rdp-total-session-count.log | Measure-Object –Line
+$rdp_total_session_count = $line_count.lines
+Get-AWSPowerShellVersion | Out-Null
+$rdp_session_count_cw = New-Object -TypeName Amazon.CloudWatch.Model.MetricDatum
+$Dimension = [Amazon.CloudWatch.Model.Dimension]::new()
+$Dimension.Name = 'RDP Session Count'
+$hostname = Get-Content env:computername
+$Dimension.Value = $hostname
+$rdp_session_count_cw.Dimensions = $Dimension
+$rdp_session_count_cw.value = $rdp_total_session_count
+$rdp_session_count_cw.MetricName = "RDP Total Session Count"
+$rdp_session_count_cw.Timestamp = [DateTime]::UtcNow
+Write-CWMetricData -Namespace CWAgent -MetricData $rdp_session_count_cw
+
+qwinsta /counter | Select-String -Pattern 'Active' > C:\rdp-active-session-count.log
+$line_count = Get-Content C:\rdp-active-session-count.log | Measure-Object –Line
+$rdp_active_session_count = $line_count.lines
+$rdp_session_count_cw = New-Object -TypeName Amazon.CloudWatch.Model.MetricDatum
+$Dimension = [Amazon.CloudWatch.Model.Dimension]::new()
+$Dimension.Name = 'RDP Session Count'
+$hostname = Get-Content env:computername
+$Dimension.Value = $hostname
+$rdp_session_count_cw.Dimensions = $Dimension
+$rdp_session_count_cw.value = $rdp_active_session_count
+$rdp_session_count_cw.MetricName = "RDP Active Session Count"
+$rdp_session_count_cw.Timestamp = [DateTime]::UtcNow
+Write-CWMetricData -Namespace CWAgent -MetricData $rdp_session_count_cw
--- get-rdp-session-count.ps1
+++ get-rdp-session-count.ps1
@@ -1,28 +1,12 @@
-qwinsta /counter | Select-String -Pattern 'Disc|Active' | Select-String -Pattern 'services|sessions' -NotMatch > C:\rdp-total-session-count.log
-$line_count = Get-Content C:\rdp-total-session-count.log | Measure-Object â€“Line
-$rdp_session_count = $line_count.lines
-Get-AWSPowerShellVersion | Out-Null
-$rdp_session_count_cw = New-Object -TypeName Amazon.CloudWatch.Model.MetricDatum
-$Dimension = [Amazon.CloudWatch.Model.Dimension]::new()
-$Dimension.Name = 'RDP Session Count'
-$hostname = Get-Content env:computername
-$Dimension.Value = $hostname
-$rdp_session_count_cw.Dimensions = $Dimension
-$rdp_session_count_cw.value = $rdp_session_count
-$rdp_session_count_cw.MetricName = "RDP Total Session Count"
-$rdp_session_count_cw.Timestamp = [DateTime]::UtcNow
-Write-CWMetricData -Namespace CWAgent -MetricData $rdp_session_count_cw
+cd C:\Scripts
+qwinsta /counter | Select-String -Pattern 'Disc|Active' | Select-String -Pattern 'services|sessions' -NotMatch > rdp-total-session-list.log
+$line_count = Get-Content rdp-total-session-list.log | Measure-Object –Line
+$rdp_total_session_count = $line_count.lines
+# echo "There are $rdp_total_session_count total RDP sessions."
+.\send-statsd.ps1 "RDPTotalSessionCount:$rdp_total_session_count|g"
 
-qwinsta /counter | Select-String -Pattern 'Active' > C:\rdp-active-session-count.log
-$line_count = Get-Content C:\rdp-active-session-count.log | Measure-Object â€“Line
-$rdp_session_count = $line_count.lines
-$rdp_session_count_cw = New-Object -TypeName Amazon.CloudWatch.Model.MetricDatum
-$Dimension = [Amazon.CloudWatch.Model.Dimension]::new()
-$Dimension.Name = 'RDP Session Count'
-$hostname = Get-Content env:computername
-$Dimension.Value = $hostname
-$rdp_session_count_cw.Dimensions = $Dimension
-$rdp_session_count_cw.value = $rdp_session_count
-$rdp_session_count_cw.MetricName = "RDP Active Session Count"
-$rdp_session_count_cw.Timestamp = [DateTime]::UtcNow
-Write-CWMetricData -Namespace CWAgent -MetricData $rdp_session_count_cw
+qwinsta /counter | Select-String -Pattern 'Active' > rdp-active-session-list.log
+$line_count = Get-Content rdp-active-session-list.log | Measure-Object –Line
+$rdp_active_session_count = $line_count.lines
+# echo "There are $rdp_active_session_count active RDP sessions."
+.\send-statsd.ps1 "RDPActiveSessionCount:$rdp_active_session_count|g"
